ab1adbecd132d65468b1df7c15a2781ee3886777
Nicholas-Darko-Brown <nicholas.brown@amalitech.org>
2021-12-13 12:09:44 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import SoundPad from './components/SoundPad/SoundPad';
+import bankOne from './util/BeatBank/BeatBank';
 
-function App() {
+
+const App = () => {
+  const [volume, setVolume] = useState(1)
+  const [record, setRecord] = useState('')
+
+  const playRecord = () => {
+
+    let index = 0
+    let recordArr = record.split(" ")
+
+    const interval = setInterval(() => {
+        const audio = document.getElementById(recordArr[index])
+        audio.time = 0
+        audio.play()
+        audio.volume = volume
+        index++
+      }, 300)
+
+      setTimeout(() => clearInterval(interval), 300 * recordArr.length - 1)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine" className="App container fw-bold text-dark mt-5 shadow-lg p-3 mb-5 bg-info rounded">
+      <div id="display">
+        <h1>Drum Machine</h1> 
+          <div>
+            {bankOne.map((bank) => {
+              return <SoundPad key={bank.id} bank={bank} volume={volume} setRecord={setRecord} />
+            })}
+          </div>
+
+          <div>
+            <input type='range' value={volume} onChange={(e) => setVolume(e.target.value)} max='1' min='0' step='0.1' />
+          </div>
+
+          <div> {record} </div>
+
+          <div>
+            {record && (
+              <div>
+                <button className='btn btn-success m-4' onClick={playRecord}>Play</button>
+                <button className='btn btn-danger' onClick={() => setRecord('')}>Delete</button>
+              </div>
+            )}
+          </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/SoundPad/SoundPad.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react"
+
+
+const SoundPad = ({ bank, volume, setRecord }) => {
+
+    const [color, setColor] = useState(false)
+  
+    useEffect(() => {
+      document.addEventListener('keydown', handleKeyPress)
+      return () => {
+        document.removeEventListener('keydown', handleKeyPress)
+      }
+    })
+  
+    const handleKeyPress = e => {
+      if(e.keyCode === bank.keyCode) {
+        sound()
+      }
+    }
+    
+  
+    const sound = () => {
+      const audio = document.getElementById(bank.keyTrigger)
+      setColor(true)
+      setTimeout(() => setColor(false), 200)
+      audio.time = 0
+      audio.play()
+      audio.volume = volume
+      setRecord(prev => prev + bank.keyTrigger + ' ')
+    }
+  
+    return (
+      <div id={bank.id} className={`btn btn-secondary p-4 m-3 drum-pad ${color && "btn-warning"}` } onClick={sound}>
+        <audio id={bank.keyTrigger} src={bank.url} className="clip" />
+        {bank.keyTrigger}
+      </div>
+    )
+  }
+
+  export default SoundPad;
